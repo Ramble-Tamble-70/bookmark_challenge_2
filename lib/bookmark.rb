c42b94bb50e7e8ff35811ccd3f38d9b3410c0ee7
David Ewan Campbell <ewan.david@gmail.com>
2022-04-15 12:49:54 +0100

    All functionalities passing & working up to step 12 of bookmark challenge

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -29,7 +29,9 @@ class Bookmark
       connection = PG.connect(dbname: 'bookmark_manager')
     end
 
-    result = connection.exec_params("INSERT INTO bookmarks (url,title) VALUES($1, $2) RETURNING id, title, url;", [url, title])
+    result = connection.exec_params(
+      "INSERT INTO bookmarks (url,title) VALUES($1, $2) RETURNING id, title, url;", [url, title]
+      )
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 end
